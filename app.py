7be6212dbb44313eacbb2f39b541d89d7ec5a148
Ally-dac <alliyahdacosta8@gmail.com>
2026-09-16 19:59:46 -0500

    task 3: online

--- app.py
+++ app.py
@@ -41,3 +41,13 @@ async def get_readings():
 @app.get("/devices/hottest")
 async def get_hottest():
     return hottest(readings)
+
+@app.get("/devices/online")
+async def get_online_devices():
+    online_devices = []
+
+    for device in readings:
+        if device["online"] == True:
+            online_devices.append(device)
+
+    return online_devices
